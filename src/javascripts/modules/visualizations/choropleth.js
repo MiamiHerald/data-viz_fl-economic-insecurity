295cd6f75837d45f986df96e19ed2e4c5b530e9c
Chris Williams <cwilliams@miamiherald.com>
2016-09-28 17:10:47 -0400

    Added legend

--- src/javascripts/modules/visualizations/choropleth.js
+++ src/javascripts/modules/visualizations/choropleth.js
@@ -17,12 +17,6 @@ class Choropleth {
     this.mapWidth = this.width;
     this.shapeUrl = `data/florida-counties.json`;
     this.rateById = d3.map();
-    this.quantizePositive = d3.scaleQuantize()
-      .domain([-30, 30])
-      .range(d3.range(9).map((i) => `p${i}-9` ));
-    this.quantizeNegative = d3.scaleQuantize()
-      .domain([-25, 15])
-      .range(d3.range(9).map((i) => `n${i}-9` ));
     this.pymChild = null;
   }
 
@@ -81,6 +75,14 @@ class Choropleth {
         });
     };
 
+    this.extent = d3.extent(this.rateById.values(), (d) => +d);
+    this.quantizePositive = d3.scaleQuantize()
+      .domain(this.extent)
+      .range(d3.range(9).map((i) => `p${i}-9` ));
+    this.quantizeNegative = d3.scaleQuantize()
+      .domain(this.extent)
+      .range(d3.range(9).map((i) => `n${i}-9` ));
+
     this.projection = d3.geoEquirectangular()
       .fitSize([this.width, this.height], topojson.feature(shapeData, shapeData.objects[`florida-counties`]));
     this.path = d3.geoPath()
@@ -127,12 +129,25 @@ class Choropleth {
           this.tooltip
             .classed(`is-active`, false);
         });
+
+    this.drawLegend();
   }
 
   draWTooltip() {
     this.tooltip = d3.select(this.el)
       .append(`div`)
       .attr(`class`, `choropleth__tooltip`);
+  }
+
+  drawLegend() {
+    const legendString = `
+      <div class="legend">
+        <p class="legend__value">${this.extent[0]}%</p>
+        <div class="legend__scale"></div>
+        <p class="legend__value">${this.extent[1]}%</p>
+      </div>`;
+
+    $(this.el).append(legendString);
   }
 }
 
