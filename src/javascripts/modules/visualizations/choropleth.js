8ec5a064752536a62f4f481a079665af6c2734b9
Chris Williams <cwilliams@miamiherald.com>
2016-10-10 13:22:22 -0400

    Updated legend and tooltips

--- src/javascripts/modules/visualizations/choropleth.js
+++ src/javascripts/modules/visualizations/choropleth.js
@@ -88,7 +88,7 @@ class Choropleth {
       .enter().append(`path`)
         .attr(`class`, (d) => {
           if (this.rateById.get(d.properties.county) === ``) {
-            return `county county--null`
+            return `county county--${d.id} county--null`
           } else {
             return `county county--${d.id}`
           }
@@ -106,7 +106,7 @@ class Choropleth {
         })
         .on(`mousemove`, () => {
           this.tooltip
-            .style(`top`, `${d3.event.pageY}px`)
+            .style(`top`, `${d3.event.pageY - 10}px`)
             .style(`left`, `${d3.event.pageX}px`);
         })
         .on(`mouseout`, (d) => {
